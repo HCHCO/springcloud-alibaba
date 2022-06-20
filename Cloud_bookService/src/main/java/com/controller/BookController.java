00628702b285fad1f044abf1e1fe6b8529c39225
Shop <2421864719@qq.com>
2022-06-20 17:22:49 +0800

    D9-nacos-config

--- Cloud_bookService/src/main/java/com/controller/BookController.java
+++ Cloud_bookService/src/main/java/com/controller/BookController.java
@@ -2,6 +2,8 @@ package com.controller;
 
 import entity.Book;
 import com.service.BookService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,12 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 
 @RestController
+@RefreshScope
 public class BookController {
     @Resource
     BookService bookService;
+    @Value("${test.fkd}")
+    String test;
     @RequestMapping("/book/{bid}")
     Book findBookById(@PathVariable("bid") int bid){
         System.out.println("hello world");
+        System.out.println(test);
         return bookService.getBookById(bid);
     }
 

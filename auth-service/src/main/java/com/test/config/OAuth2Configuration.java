04a3788fe0a750ade267a9514e78ba9b34b3f35f
Shop <2421864719@qq.com>
2022-07-09 16:46:53 +0800

    D15-Ouath2

---
+++ auth-service/src/main/java/com/test/config/OAuth2Configuration.java
@@ -0,0 +1,46 @@
+package com.test.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+@EnableAuthorizationServer
+public class OAuth2Configuration extends AuthorizationServerConfigurerAdapter {
+    @Resource
+    private AuthenticationManager manager;
+
+    private final BCryptPasswordEncoder encoder=new BCryptPasswordEncoder();
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients)throws Exception{
+        clients
+                .inMemory()
+                .withClient("web")
+                .secret(encoder.encode("654321"))
+                .autoApprove(false)
+                .scopes("book","user","borrow")
+                .authorizedGrantTypes("client_credentials","password","implicit","authorization_code","refresh_token");
+
+    }
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security){
+        security
+                .passwordEncoder(encoder)
+                .allowFormAuthenticationForClients()
+                .checkTokenAccess("permitAll()");
+    }
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints){
+        endpoints
+                .authenticationManager(manager);
+    }
+}
